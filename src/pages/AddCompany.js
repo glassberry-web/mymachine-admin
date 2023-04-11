85b20ed49ea0abd33ca227dcdf3c33fd84c65582
Aviinaaash <aviinaashdevkate@gmail.com>
2023-04-11 11:59:45 +0530

    id removed again

--- src/pages/AddCompany.js
+++ src/pages/AddCompany.js
@@ -2,7 +2,7 @@ import React from "react";
 import SideBar from "../utils/SideBar";
 
 function AddVendor() {
-  const { _id } = JSON.parse(localStorage.getItem("superAdminvendorid"));
+  // const { _id } = JSON.parse(localStorage.getItem("superAdminvendorid"));
 
   return (
     <div className="page-content">
