788a03ace82cbff45a00914132110cefce6eee86
Aviinaaash <aviinaashdevkate@gmail.com>
2023-05-06 21:51:42 +0530

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const baseURL = "https://my-machine-store-dashboardapi.onrender.com";
-const baseURL = "http://localhost:5001";
-// const baseURL = "http://15.207.31.23:5001";
+// const baseURL = "http://localhost:5001";
+const baseURL = "http://15.207.31.23:5001";
 export default axios.create({
   baseURL: baseURL,
   withCredentials: true
