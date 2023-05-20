28211caf78cbf30c7f6dc7a556c7d796403e5beb
Aviinaaash <aviinaashdevkate@gmail.com>
2023-05-20 11:02:20 +0530

    axios modified

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // const baseURL = "https://my-machine-store-dashboardapi.onrender.com";
-// const baseURL = "http://localhost:5001";
-const baseURL = "https://mymachinestore.com/enquiry";
+const baseURL = "http://15.207.31.23:5001";
+// const baseURL = "https://mymachinestore.com/enquiry";
 export default axios.create({
   baseURL: baseURL,
   withCredentials: true
 });
-// const apiUrl = 'http://localhost:5001';
+// const apiUrl = 'http://localhost:5001';git
 // export default axios.interceptors.request.use(
 //   config => {
 //     const { origin } = new URL(config.url);
