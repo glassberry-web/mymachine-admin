592fe9a0c7f670e2b7f4923076b5e5fd9f79ff93
omtapse <omtapse01@gmail.com>
2023-07-08 13:51:49 +0530

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = "https://my-machine-store-dashboardapi.onrender.com";
 // const baseURL = "http://15.207.31.23:5001";
-const baseURL = "http://localhost:5000"
+const baseURL = "http://194.31.53.145:5000"
 // const baseURL = "https://mymachinestore.com/enquiry";
 export default axios.create({
   baseURL: baseURL,
