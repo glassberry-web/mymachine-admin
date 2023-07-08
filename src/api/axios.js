ab4c2aa4230f5f8136086ca8ef52765a1a4df1e7
omtapse <omtapse01@gmail.com>
2023-07-08 15:20:08 +0530

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 // const baseURL = "https://my-machine-store-dashboardapi.onrender.com";
 // const baseURL = "http://15.207.31.23:5001";
-const baseURL = "http://194.31.53.145:5000"
+const baseURL = "http://localhost:5000"
 // const baseURL = "https://mymachinestore.com/enquiry";
 export default axios.create({
   baseURL: baseURL,
-  withCredentials: true
+  // withCredentials: true
 });
 // const apiUrl = 'http://localhost:5001';git
 // export default axios.interceptors.request.use(
